6f522f21f415b48613d8ade209a3fd842997ae49
Cewena <celia-am@hotmail.fr>
2018-02-21 15:05:07 +0100

    Update Application.java

--- Jeu 2048/src/application/Application.java
+++ Jeu 2048/src/application/Application.java
@@ -1,14 +1,12 @@
 package application;
 import jeu_2048_de_farida.*;
 
-
 public class Application {
 	
 	private static FenJeu2048DeFarida f;
 
 	public static void main(String [] args){
 		setF(new FenJeu2048DeFarida("2048"));
-		
 	}
 
 	public static FenJeu2048DeFarida getF() {
