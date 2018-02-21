53a3c3c28a1419756ae41f7b267e4d4e36750958
Cewena <celia-am@hotmail.fr>
2018-02-21 15:00:41 +0100

    Update PanelCentreTest.java

--- Jeu 2048/src/jeu_2048_de_farida/graph/PanelCentreTest.java
+++ Jeu 2048/src/jeu_2048_de_farida/graph/PanelCentreTest.java
@@ -12,15 +12,11 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
-
 import javax.swing.JPanel;
-
 import moteur.*;
-
 
 public class PanelCentreTest extends JPanel implements ColorsInterface,MouseListener,MouseMotionListener {
 		
-	
 	private static final long serialVersionUID = 1L;
 	ArrayList<Point> p;
 	private Graphics g;
@@ -47,17 +43,8 @@ public class PanelCentreTest extends JPanel implements ColorsInterface,MouseList
 	}
 	
 	private void ajoutePoint(ArrayList<Point> p){
-		/*for(int i =0;i<p.size();i++){
-			for(int j =0;j<p.size();j++){
-				this.g.drawLine((int)p.get(i).getX(),(int) p.get(i).getY(),(int)p.get(j).getX(), (int)p.get(j).getY());
-				g.drawRect(0, 0, 100, 100);
-				
-			}
-		}*/
 		if(this.p.size() >2){
-			for(int i = 2; i< p.size();i++){
-					
-				
+			for(int i = 2; i< p.size();i++){	
 				Point un = this.p.get(i-2);
 				Point deux = this.p.get(i-1);
 				g.drawLine((int)un.getX(), (int)un.getY(), (int)deux.getX(), (int) deux.getY() );
@@ -66,37 +53,29 @@ public class PanelCentreTest extends JPanel implements ColorsInterface,MouseList
 		repaint();
 	}
 	
-	
-
-		
-
 		
 		public void mouseClicked(MouseEvent e) {
-	
-			
+		
 		}
+	
 		public void mouseEntered(MouseEvent e) {
 			
-			
-			
 		}
-		public void mouseExited(MouseEvent e) {
 	
+		public void mouseExited(MouseEvent e) {
 			
 		}
+	
 		public void mousePressed(MouseEvent e) {
 			this.p.add(e.getPoint());
 
 			repaint();
-			this.pressed = true;
-			
+			this.pressed = true;	
 		}
+	
 		public void mouseReleased(MouseEvent e) {
 			this.pressed = false;
-			
-			
-		
-	}
+		}
 
 		public void mouseDragged(MouseEvent e) {
 			if(this.pressed){
